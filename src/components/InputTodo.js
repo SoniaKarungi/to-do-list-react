3fd904f5057a22694d766fb46c4596a87b05f272
SoniaKarungi <98121137+SoniaKarungi@users.noreply.github.com>
2022-06-02 09:35:29 +0300

    Add styling
    Part 3 – How to implement CSS in Reactjs app.

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -24,15 +24,18 @@ class InputTodo extends Component {
   };
 
   render() {
+    const { title } = this.state;
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} className="form-container">
         <input
           type="text"
+          className="input-text"
           placeholder="Add todo..."
-          value={this.state.title}
+          value={title}
+          name="title"
           onChange={this.onChange}
         />
-        <button>Submit</button>
+        <button className="input-submit">Submit</button>
       </form>
     );
   }
